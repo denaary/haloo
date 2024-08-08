2c612b716bb0d4b9649caad522524c79ebec62d9
denaary <cilacapdenaary@gmail.com>
2024-08-08 19:37:44 +0700

    haloo

---
+++ script.js
@@ -0,0 +1,11 @@
+function nextPage() {
+    window.location.href = "message.php";
+}
+
+function next() {
+    window.location.href = "next.php";
+}
+
+function last() {
+    window.location.href = "last.php";
+}
